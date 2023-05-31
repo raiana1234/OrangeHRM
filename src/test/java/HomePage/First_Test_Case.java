264e24ca21adf93ed6c81f28e901973e715f8073
Raiana Razzaq <raianarazzaq@gmail.com>
2023-05-30 22:13:10 -0700

    First_Test_Case.java

---
+++ src/test/java/HomePage/First_Test_Case.java
@@ -0,0 +1,35 @@
+package HomePage;
+
+import org.testng.annotations.Test;
+
+//How to delet the old testng.xml
+public class First_Test_Case {
+
+    @Test
+    void Hasan() {
+
+        System.out.println("Hasan");
+    }
+
+    @Test
+    void testing() {
+
+        System.out.println("Raiana");
+
+
+    }
+
+    @Test
+    void Hasan2() {
+
+        System.out.println("Hasan2");
+    }
+
+    @Test
+    void testing2() {
+
+        System.out.println("Raiana2");
+
+
+    }
+}
