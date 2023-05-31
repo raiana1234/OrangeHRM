a8cd7b48cace79997a01254727c032ddfe7457a6
Raiana Razzaq <raianarazzaq@gmail.com>
2023-05-30 22:11:53 -0700

    CheckBox.java

--- src/test/java/HomePage/CheckBox.java
+++ src/test/java/HomePage/CheckBox.java
@@ -21,25 +21,24 @@ public class CheckBox {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 
-
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scroll(0,document.body.scrollHeight/3)");
 
         // selecting single checkbox
 //        driver.findElement(By.xpath("//input[@id='monday']")).click();
 //        driver.findElement(By.xpath("//input[@id='tuesday']")).click();
-        List<WebElement> allCheckboxItems=driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
-        System.out.println("Total number of days are: "+ allCheckboxItems.size());
+        List<WebElement> allCheckboxItems = driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+        System.out.println("Total number of days are: " + allCheckboxItems.size());
 
         //Select all the checkboxes
-        for(int i=0; i<allCheckboxItems.size(); i++){
-        allCheckboxItems.get(i).click();
+        for (int i = 0; i < allCheckboxItems.size(); i++) {
+            allCheckboxItems.get(i).click();
         }
 
         Thread.sleep(2000);
 
         //Select single check box by x-path and click
-           //driver.findElement(By.xpath("//input[@id='male']")).click();
+        //driver.findElement(By.xpath("//input[@id='male']")).click();
 //            In the provided code, the continue statement is used inside the for loop with the condition if (i==3 || i==4)
 //        to skip the execution to the click() method for checkboxes at indexes 3 and 4
 //        int a = 0;
@@ -47,10 +46,10 @@ public class CheckBox {
 //        }
         //allCheckboxItems.get(a).click();
 
-        for (int x =0; x < allCheckboxItems.size(); x++){
+        for (int x = 0; x < allCheckboxItems.size(); x++) {
 
-            WebElement checkbox =allCheckboxItems.get(x);
-            if (checkbox.isSelected()&& (x !=3 && x !=6)){
+            WebElement checkbox = allCheckboxItems.get(x);
+            if (checkbox.isSelected() && (x != 3 && x != 6)) {
                 checkbox.click();
             }
         }
